eac00791b368b6109e170f62d45d901c59a408bc
Glitch (react-native-web-playground) <none>
2020-11-07 06:19:18 +0000

    ❣️🎿 Checkpoint ./src/header.js:789040/450

---
+++ src/header.js
@@ -0,0 +1,9 @@
+import Component from 'React'
+
+export default class Header extends Component{
+  // const address = this.props
+  
+  render() {
+    return <a href={address} target='_blank' />
+  }
+}
