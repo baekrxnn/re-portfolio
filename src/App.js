e9dd58ab67deccd6cf867d383765ecc7cb3c9f21
Glitch (react-native-web-playground) <none>
2020-12-22 00:31:00 +0000

    ☎️🍲 Checkpoint ./src/App.js:789040/1625 ./src/App.css:789040/205 ./src/sidebar.js:789040/108

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
-import Sidebar from './sidebar.js';
-import Weblink from './webLinks.js'
+import React, { Component } from "react";
+import "./App.css";
+import Sidebar from "./sidebar.js";
+
 // import logo from 'logo.svg';
 // import { Image, StyleSheet, Text, View } from 'react-native';
-
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Sidebar />
-        <h1>Karen He</h1>
-        <Weblink address='https://github.com/baekrxnn' name='Github'/>
+        <div classname="sidebar">
+          <Sidebar />
+        </div>
+        
+        <div>
+          <h1>Karen He</h1>
+        </div>
       </div>
-    )
+    );
   }
 }
-
 
 export default App;
--- src/sidebar.js
+++ src/sidebar.js
@@ -4,7 +4,10 @@ import Weblink from './webLinks.js';
 export default class Sidebar extends Component{
   render(){
     // let items = this.props.sidebarItems
-    let items = [<Weblink address='https://github.com/baekrxnn' name='Github'/>]
+    let items = [
+      <Weblink address='https://github.com/baekrxnn' name='Github'/>
+      <Weblink address='https://www.linkedin.com/in/karen18he/' name='Lin'
+    ]
     let listItems = []
     items.forEach(link => listItems.push(<li> {link} </li>))
     return (
