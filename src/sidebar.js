f83f1596e5de8a8c069a3a71aa43703370f0e5d3
Glitch (react-native-web-playground) <none>
2020-12-22 00:19:36 +0000

    🏫🌓 Checkpoint ./src/sidebar.js:789040/609 ./src/App.js:789040/2

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Weblink from './webLinks.js';
 
 export default class Sidebar extends Component{
   render(){
+    // let items = this.props.sidebarItems
+    let items = [<Weblink address='https://github.com/baekrxnn' name='Github'/>]
+    let listItems = []
+    items.forEach(link => listItems.push(<li> {link} </li>))
     return (
       <ul>
-        <li> one </li>
-        <li> two </li>
+        {listItems}
       </ul>
     )
   }
