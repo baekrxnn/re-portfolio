53647cb349fdaf404cbec0c4cbe4cf176a4ff42c
Glitch (re-portfolio) <none>
2020-12-24 05:36:59 +0000

    ⛩🎯 Checkpoint ./src/about.css:789040/737 ./public/index.html:789040/40 ./src/projects.js:789040/97 ./src/App.css:789040/278 ./src/App.js:789040/1080

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         <div className="sidebar">
           <Sidebar />
         </div>
-        
+
         <div className="contents">
           <AboutMe />
         </div>
