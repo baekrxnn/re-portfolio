c2d009312685bf18d94d85fff2dbfbe998ed0d33
Glitch (re-portfolio) <none>
2020-12-25 02:06:54 +0000

    🏉📰 Checkpoint ./src/about.js:789040/2579 ./src/about.css:789040/21

--- src/about.js
+++ src/about.js
@@ -1,41 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./about.css";
 
-export default class about extends Component{
+export default class about extends Component {
   render() {
-    let codingTechs = [ // because I expect this list to grow
-      'CSS',
-      'HTML',
-      'Java',
-      'JavaScript (React, jQuery)',
-      'Python',
-    ]
-    
-    let codeItems = []
-    for (let i=0; i<codingTechs.length; i++) {
-      codeItems.push(<li key={i}> {codingTechs[i]} </li>)
+    let codingTechs = [
+      // because I expect this list to grow
+      "CSS",
+      "HTML",
+      "Java",
+      "JavaScript (React, jQuery)",
+      "Python"
+    ];
+
+    let codeItems = [];
+    for (let i = 0; i < codingTechs.length; i++) {
+      codeItems.push(<li key={i}> {codingTechs[i]} </li>);
     }
-    
-    return(
+
+    return (
       <div>
         <h1 className="name"> Karen He</h1>
-        
+
         <div className="bio">
           <ul>
             <li> First year Computer Science student at University of California Irvine </li>
             <li> Previous software engineer intern at LaunchDarkly (June - September, 2020) </li>
+            <li> Google Computer Science Summer Institute participant (July, 2020) </li>
           </ul>
         </div>
-        
+
         <div className="languages">
           <h3> Coding languages and libraries: </h3>
-          <ul>
-            {codeItems}
-          </ul>
-        
+          <ul>{codeItems}</ul>
         </div>
-      
       </div>
-    )
+    );
   }
 }
