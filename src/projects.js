9540189f758047d1b93be947b55ecbccce6fafb9
Glitch (re-portfolio) <none>
2021-05-04 05:54:25 +0000

    🐯🚍 Checkpoint ./src/projects.js:789040/1190

--- src/projects.js
+++ src/projects.js
@@ -2,21 +2,25 @@ import React, { Component } from "react";
 import projectsData from "./projects.json";
 import Weblink from "./webLinks.js";
 
-const weblinks = () => {
-  let currentProjects = projectsData.Projects;
-  currentProjects.forEach(x => {
-    return <Weblink address={x.link} name={x.name} />
-  });
-};
+// const weblinks = () => {
+//   let currentProjects = projectsData.Projects;
+//   currentProjects.forEach(x => {
+//     return <Weblink address={x.link} name={x.name} /> 
+//   });
+// };
 
 const projects = () => {
   // console.log(projectsData.Projects);
-  links = projectsData.Projects.forEach
+  projectsData.Projects.forEach(x => console.log(x.name));
+  let links = [];
+  projectsData.Projects.forEach(x => links.push(<Weblink address={x.link} name={x.name} key={x.name}/>));
+  // let one = projectsData.Projects[0];
+  // let rOne = (<Weblink address={one.link} name={one.name} />);
 
   return (
     <div>
       <p>{projectsData.Year}</p>
-      {weblinks()}
+      {links}
     </div>
   );
 };
